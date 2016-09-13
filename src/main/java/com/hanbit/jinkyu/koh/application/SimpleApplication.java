50755ae6c7f137a45c0c9cbf46c74aea7ef94e94
hb5027 <hb5027@hb5027-PC>
2016-09-13 12:32:55 +0900

    (2016-09-13) commit 1st

---
+++ src/main/java/com/hanbit/jinkyu/koh/application/SimpleApplication.java
@@ -0,0 +1,23 @@
+package com.hanbit.jinkyu.koh.application;
+
+import com.hanbit.jinkyu.koh.core.service.SchedulerService;
+import com.hanbit.jinkyu.koh.core.vo.ScheduleVO;
+
+public class SimpleApplication {
+
+	public static void main(String[] args) {
+		SchedulerService schedulerService = new SchedulerService();
+
+		ScheduleVO schedule = new ScheduleVO();
+		schedule.setScheduleId(String.valueOf(System.currentTimeMillis()));
+		schedule.setTitle("저녁");
+		schedule.setMemo("반찬 뭘까");
+		schedule.setStartDt("201609131830");
+		schedule.setEndDt("201609131930");
+
+		int result = schedulerService.addSchedule(schedule);
+
+		System.out.println("결과: " + result);
+	}
+
+}
